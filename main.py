d9b2b040c6c10028487f91a03268311192d21478
sschimper <sebastianschimper@gmail.com>
2020-06-28 18:28:45 +0200

    implemented rotation, coldnt check if it works

--- main.py
+++ main.py
@@ -4,10 +4,33 @@ import math
 
 
 # rotation matrix (rotates counter-clockwise around x axis)
-def rotate(vector, theta):
-    rot_matrix = np.array([[1, 0, 0],
-                           [0, math.cos(theta), -math.sin(theta)],
-                           [0, math.sin(theta), math.cos(theta)]
+def rotateX(vector, theta):
+    rot_matrix = np.array([[1, 0, 0, 0],
+                           [0, math.cos(theta), -math.sin(theta), 0],
+                           [0, math.sin(theta), math.cos(theta), 0],
+                           [0, 0, 0, 1]
+                           ])
+
+    return rot_matrix.dot(vector)
+
+
+# rotation matrix (rotates counter-clockwise around y axis)
+def rotateY(vector, theta):
+    rot_matrix = np.array([[math.cos(theta), 0, math.sin(theta), 0],
+                           [0, 1, 0, 0],
+                           [-math.sin(theta), 0, math.cos(theta), 0],
+                           [0, 0, 0, 1]
+                           ])
+
+    return rot_matrix.dot(vector)
+
+
+# rotation matrix (rotates counter-clockwise around z axis)
+def rotateZ(vector, theta):
+    rot_matrix = np.array([[math.cos(theta), -math.sin(theta), 0, 0],
+                           [math.sin(theta), math.cos(theta), 0, 0],
+                           [0, 0, 1, 0],
+                           [0, 0, 0, 1]
                            ])
 
     return rot_matrix.dot(vector)
@@ -16,23 +39,31 @@ def rotate(vector, theta):
 # representation of frame of reference
 # using right-handed coordinate system
 class ReferenceFrame:
-    def __init__(self, angle):
+    def __init__(self, angleX, angleZ):
         # initial coordinate system
-        self.x = np.array([1, 0, 0])
-        self.y = np.array([0, 1, 0])
-        self.z = np.array([0, 0, 1])
+        self.x = np.array([1, 0, 0, 0])
+        self.y = np.array([0, 1, 0, 0])
+        self.z = np.array([0, 0, 1, 0])
+        self.w = np.array([0, 0, 0, 1])
 
-        # rotate frame
-        self.x = rotate(self.x, angle)  # should be the same
-        self.y = rotate(self.y, angle)
-        self.z = rotate(self.z, angle)
+        # rotate frame around x axis
+        if angleX is not None and angleZ is None:
+            self.x = rotateX(self.x, angleX)  # should be the same
+            self.y = rotateX(self.y, angleX)
+            self.z = rotateX(self.z, angleX)
+            self.w = rotateX(self.w, angleX)
+
+        # rotate frame around Z axis
+        elif angleZ is not None and angleZ is not None:
+            self.x = rotateX(self.x, angleX) * rotateZ(self.x, angleZ)  # should be the same
+            self.y = rotateX(self.y, angleX) * rotateZ(self.y, angleZ)
+            self.z = rotateX(self.z, angleX) * rotateZ(self.z, angleZ)
+            self.w = rotateX(self.w, angleX) * rotateZ(self.w, angleZ)
 
         # verify orthogonality
         assert self.x.dot(self.y) == 0.0 and self.x.dot(self.z) == 0.0 and self.y.dot(self.x) == 0.0
-        print("x : ", self.x)
-        print("y : ", self.y)
-        print("z : ", self.z)
 
+    # getters and setters
     def get_x(self):
         return self.x
 
@@ -95,7 +126,7 @@ class MuellerMatrix:
                                 [0, 0, 0, 0]])
 
     # calculate the mueller matrix
-    def set_mueller_matriy(self, matrix):
+    def set_mueller_matrix(self, matrix):
         self.matrix = matrix
 
     # set entry frame
@@ -107,11 +138,119 @@ class MuellerMatrix:
         self.frame_exit = frame_exit
 
 
-# get mueller matrix for reflection
-def get_reflection_muller_matrix():
-    t_fresnel = MuellerMatrix(None, None)
+# class for representing filter
+class PolarizationFilter:
+    def __init__(self, matrix, frame):
+        self.matrix = matrix
+        self.frame = frame
 
-    return t_fresnel
+    # calculate the mueller matrix
+    def set_matrix(self, matrix):
+        self.matrix = matrix
+
+    # set frame
+    def set_entry_frame(self, frame):
+        self.frame = frame
+
+
+# class for storing complex number
+class ComplexNumber:
+    def __init__(self, real, imag):
+        self.real = real
+        self.imag = imag
+
+
+# calculate a coeff
+def calculate_a_coeff(ior_c, theta):
+    a = math.sqrt((1 / 2) * math.sqrt((ior_c.real ** 2 - ior_c.imag ** 2 -
+                                       math.sin(theta) ** 2) ** 2 + 4 * ior_c.real ** 2 * ior_c.imag ** 2)
+                  + ior_c.real ** 2 - ior_c.imag ** 2 - math.sin(theta) ** 2)
+    return a
+
+
+# calculate b coeff
+def calculate_b_coeff(ior_c, theta):
+    first_part = math.sqrt(0.5)
+    second_part = math.sqrt(
+        (ior_c.real ** 2 - ior_c.imag ** 2 - math.sin(theta) ** 2) ** 2 + 4 * ior_c.real ** 2 * ior_c.imag ** 2)
+    third_part = - ior_c.real ** 2 + ior_c.imag ** 2 + math.sin(theta) ** 2
+
+    return first_part * second_part + third_part
+
+
+# calculate the mueller matrix
+def calculate_mueller_matrix(ior_c, a, b, theta):
+    f_perp = (a ** 2 + b ** 2 - 2 * a * math.cos(theta) + math.cos(theta) ** 2) / (
+            a ** 2 + b ** 2 + 2 * a * math.cos(theta) + math.cos(theta) ** 2)
+    f_paral = (a ** 2 + b ** 2 - 2 * a * math.sin(theta) * math.tan(theta) + (math.sin(theta) ** 2) * (
+            math.tan(theta) ** 2)) / \
+              (a ** 2 + b ** 2 + 2 * a * math.sin(theta) * math.tan(theta) + (math.sin(theta) ** 2) * (
+                      math.tan(theta) ** 2)) * f_perp
+    tan_delta_perp = (2 * b * math.cos(theta)) / ((math.cos(theta) ** 2) - a ** 2 - b ** 2)
+    tan_delta_paral = (2 * math.cos(theta) * (
+            (ior_c.real ** 2 - ior_c.imag ** 2) * b - 2 * ior_c.real * ior_c.imag * a)) / \
+                      ((ior_c.real ** 2 + ior_c.imag ** 2) ** 2 * (math.cos(theta) ** 2) - a ** 2 - b ** 2)
+
+    A = (f_perp + f_paral) / 2
+    B = (f_perp - f_paral) / 2
+    C = math.cos(tan_delta_perp - tan_delta_paral) * math.sqrt(f_perp * f_paral)
+    S = math.sin(tan_delta_perp - tan_delta_paral) * math.sqrt(f_perp * f_paral)
+
+    matrix = np.array([[A, B, 0, 0],
+                       [B, A, 0, 0],
+                       [0, 0, C, S],
+                       [0, 0, -S, C]])
+    return matrix
+
+
+# calculate polarization matrix
+def calculate_polarization_matrix(gamma):
+    matrix = np.array([[1, math.cos(2 * gamma), math.sin(2 * gamma), 0],
+                       [math.cos(2 * gamma), math.cos(2 * gamma) ** 2, math.sin(2 * gamma) * math.cos(2 * gamma), 0],
+                       [math.sin(2 * gamma), math.sin(2 * gamma) * math.cos(2 * gamma), math.sin(2 * gamma) ** 2, 0],
+                       [0, 0, 0, 0]])
+    return matrix
+
+
+# check if 2 reference frames are matching
+def referenceframe_match(a, b):
+    if np.all(a.x == b.x) and np.all(a.y == b.y) and np.all(a.z == a.z):
+        return True
+    else:
+        return False
+
+
+# calculate magnitude
+def calculate_magnitude(vector):
+    return np.sqrt(vector[0] ** 2 + vector[1] ** 2 + vector[2] ** 2)
+
+
+# calculate angle btw two vectors
+def calculate_angle(vec_a, vec_b):
+    vec_a_mag = calculate_magnitude(vec_a)
+    vec_b_mag = calculate_magnitude(vec_b)
+
+    res = np.dot(vec_a, vec_b) / np.dot(vec_a_mag, vec_b_mag)
+    res = np.arccos(res)
+    return res
+
+
+# rotate reference frame to match mueller matrix entry frame
+def rotate_referenceframe(reference_frame, entry_matrix):
+    theta_x = calculate_angle(reference_frame.x, entry_matrix.x)
+    theta_y = calculate_angle(reference_frame.y, entry_matrix.y)
+    theta_z = calculate_angle(reference_frame.z, entry_matrix.z)
+
+    assert theta_x == theta_y and theta_y == theta_z
+
+    rotateX(reference_frame.x, theta_x)
+    rotateY(reference_frame.y, theta_y)
+    rotateZ(reference_frame.z, theta_z)
+
+    assert referenceframe_match(reference_frame, entry_matrix)
+
+    # verify orthogonality
+    assert reference_frame.x.dot(reference_frame.y) == 0.0 and reference_frame.x.dot(reference_frame.z) == 0.0 and reference_frame.y.dot(reference_frame.x) == 0.0
 
 
 # print the resulting vector
@@ -120,10 +259,13 @@ def print_stokes_vector(sv):
     print(sv)
 
 
-def do_stuff(points_conductor, ior, ext_coeff, delta, rho, phi, use_polarization_filter):
+# main calculation function
+def calculate_polarized_light(points_conductor, ior_c, ext_coeff, delta, rho, phi, use_polarization_filter):
     print("\n=============================================")
     print("\nUsing Conductor: ", points_conductor)
-    print("Index of Refraction: ", ior)
+    if ior_c is not None:
+        print("Index of Refraction - Real Part: ", ior_c.real)
+        print("Index of Refraction - Imaginary Part: ", ior_c.imag)
     if ext_coeff is not None:
         print("Extinction Coefficient: ", ext_coeff)
     print("Delta: ", delta)
@@ -133,16 +275,41 @@ def do_stuff(points_conductor, ior, ext_coeff, delta, rho, phi, use_polarization
 
     print("\nSTARTING CALCULATION")
 
-    # angle for alighning coordinate system with light beam
-    eye_rot_angle = 180 - 90 - delta
-    eye_frame = ReferenceFrame(eye_rot_angle)
+    # calculate a and b coefficitents
+    a = calculate_a_coeff(ior_c, delta)
+    b = calculate_b_coeff(ior_c, delta)
+
+    # angle for aligning coordinate system with light beam
+    eye_rot_angle = 90 - delta
+    eye_frame = ReferenceFrame(eye_rot_angle, None)
 
     stokes_vector = StokesVector(100.0, 0.0, 0.0, 0.0, eye_frame)
 
-    x1_rot_angle = - (90 - delta)
-    x1_entry_frame = ReferenceFrame(x1_rot_angle)
+    # X1 - setup
+    x1_rot_angle = (180 - 2 * delta) / 2
+    x1_entry_frame = ReferenceFrame(x1_rot_angle, None)  # same as frame of stokes vector
+    x1_exit_frame = ReferenceFrame(-x1_rot_angle, None)  # clockwise rotation
+    x1_mueller_matrix = MuellerMatrix(x1_entry_frame, x1_exit_frame)
+    x1_mueller_matrix.matrix = calculate_mueller_matrix(ior_c, a, b, delta)
 
-    x1_mueller_matrix = MuellerMatrix(eye_frame, x1_entry_frame)
+    # X2 - setup
+    x2_rot_angle = ((180 - 2 * phi) / 2)
+    x2_entry_frame = ReferenceFrame(-x2_rot_angle, -rho)
+    x2_exit_frame = ReferenceFrame(x2_rot_angle, -rho)
+    x2_mueller_matrix = MuellerMatrix(x2_entry_frame, x2_exit_frame)
+    x2_mueller_matrix.matrix = calculate_mueller_matrix(ior_c, a, b, phi)
+
+    # X1 - interaction
+    if referenceframe_match(stokes_vector.frame, x1_mueller_matrix.frame_entry):
+        print_stokes_vector(stokes_vector.vector)
+        stokes_vector.vector = x1_mueller_matrix.matrix.dot(stokes_vector.vector)
+        print_stokes_vector(stokes_vector.vector)
+
+    # filter - setup
+    pol_filter = PolarizationFilter(None, None)
+    if use_polarization_filter:
+        pol_filter.set_matrix(calculate_polarization_matrix(4))
+        pol_filter.set_entry_frame(x2_exit_frame)
 
 
 # process user input
@@ -160,11 +327,35 @@ def get_user_parameters():
             points_conductor = True
 
     # index of refraction
-    ior = input("\nPlease enter your preferred value for the Index of Refraction: ")
-    ior = float(ior)
-    while ior < 0:
-        ior = input("Error: Index of refraction must not be negative. Please enter another value: ")
+    ior_complex_answer = input("\nWould you like the Index of Refraction to be a complex number? \n"
+                               "If so, please type 'y', if not, type anything else: ")
+
+    ior_complex_bool = False
+    if ior_complex_answer == 'y':
+        ior_complex_bool = True
+    else:
+        ior_complex_bool = False
+
+    ior_c = None
+    if not ior_complex_bool:
+        ior = input("\nPlease enter your preferred value for the Index of Refraction: ")
         ior = float(ior)
+        while ior < 0:
+            ior = input("Error: Index of refraction must not be negative. Please enter another value: ")
+            ior = float(ior)
+        ior_c = ComplexNumber(ior, 0.0)
+    else:
+        ior_real = input("\nPlease enter your preferred real value for the Index of Refraction: ")
+        ior_real = float(ior_real)
+        while ior_real < 0:
+            ior_real = input("Error: Real value must not be negative. Please enter another value: ")
+            ior_real = float(ior_real)
+        ior_imag = input("\nPlease enter your preferred imaginary value for the Index of Refraction: ")
+        ior_imag = float(ior_imag)
+        while ior_imag < 0:
+            ior_imag = input("Error: Imaginary value must not be negative. Please enter another value:  ")
+            ior_imag = float(ior_imag)
+        ior_c = ComplexNumber(ior_real, ior_imag)
 
     # extinction coefficient
     ext_coeff = None
@@ -176,15 +367,15 @@ def get_user_parameters():
             ext_coeff = float(ext_coeff)
 
     # delta angle
-    delta_answer = input("\nPlease enter your preferred value for the angle Delta: ")
+    delta_answer = input("\nPlease enter your preferred value for the angle Delta (in degrees): ")
     delta = float(delta_answer) % 360
 
     # rho angle
-    rho_answer = input("\nPlease enter your preferred value for the angle Rho: ")
+    rho_answer = input("\nPlease enter your preferred value for the angle Rho (in degrees): ")
     rho = float(rho_answer) % 360
 
     # phi angle
-    phi_answer = input("\nPlease enter your preferred value for the angle Phi: ")
+    phi_answer = input("\nPlease enter your preferred value for the angle Phi (in degrees): ")
     phi = float(phi_answer) % 360
 
     # query the user if (s)he wants to use polarization filter
@@ -193,7 +384,7 @@ def get_user_parameters():
         use_polarization_filter = True
 
     # start calculation
-    do_stuff(points_conductor, ior, ext_coeff, delta, rho, phi, use_polarization_filter)
+    calculate_polarized_light(points_conductor, ior_c, ext_coeff, delta, rho, phi, use_polarization_filter)
 
 
 # header
